8704d90c70b47e29bbc5fa56432a559fe452f1cc
zoubid <empressbida@gmail.com>
2017-08-08 11:09:54 +0100

    []:

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+	'name' : "Multiuser To-Do",
+
+	'description' : "Extend the To-do app to multiuser",
+
+	'author' : "zoubida",
+
+	'summary' : """ gestion des task """,
+
+	'depends' : ['todo-app'],
+
+	'data' : ['viewtodomult.xml']
+	
+}
